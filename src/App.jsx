04440bf9260e1a1d72476deb93ed49ce556b17a6
vel-3633f <vel3633fu@gmail.com>
2023-12-01 00:07:34 +0900

    任意の数字を追加できるボタン実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./redux/counterSlice";
-import "./App.css"
+import { decrement, increment, incrementByAmount } from "./redux/counterSlice";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
   return (
     <div className="box">
       <h1>count: {count}</h1>
-      <button onClick={()=>dispatch(increment())}>+</button>
-      <button onClick={()=>dispatch(decrement())}>-</button>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount}/>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>追加</button>
     </div>
   );
 }
